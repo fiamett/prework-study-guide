dbc6d8a83a05a48637e86b06f8c0f7622dbf1ba0
fiametta <f.ignite@hotmail.com>
2022-08-04 05:10:11 -0700

    added more javascript console stuff

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,33 @@
-var topic = ["html","css","git","javascript"];
 
-for (var x=0; x < topic.length; x++){
-    console.log (topic[x]) ;
+var topics = ["html","css","git","javascript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)]
+
+
+
+    function listTopics()
+{
+
+    for (var x=0; x < topics.length ; x++){
+        console.log (topics[x]) ;
+    }
+}
+function selectTopic() {
+
+    if (randomTopic === 'html') {
+    console.log("Let's study html!");
+    } else if (randomTopic === 'css') {
+    console.log("Let's study css!");
+    } else if (randomTopic === 'git') {
+    console.log("Let's study git!");
+    } else if (randomTopic === 'javaScript') {
+    console.log("Let's study javaScript!");
+    } else {
+    console.log('Please try again!');
+    }
+
 }
 
-/*
-if (topic === 'html') {
- console.log("Let's study html!");
-} else if (topic === 'css') {
- console.log("Let's study css!");
-} else if (topic === 'git') {
- console.log("Let's study git!");
-} else if (topic === 'javaScript') {
- console.log("Let's study javaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
+console.log("This is what we learned so far");
+listTopics();
+console.log ("what to do first...");
+selectTopic();
